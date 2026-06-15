4163ec2979003dff67b132a995a8f7e4c156e0f7
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-06-15 15:11:28 -0600

    Fix and add tests chat model
    Changes include:
    - Change to use Qwen3.5 2B as chat model with multimodal inputs
    - Add test for text and image inputs

---
+++ tests/vllm/test_chat.py
@@ -0,0 +1,52 @@
+from openai import OpenAI
+from pathlib import Path
+
+from utils import encode_image
+
+MODEL_CHAT = 'cyankiwi/Qwen3.5-2B-AWQ-4bit'
+URL_CHAT = 'http://127.0.0.1:8000/v1'
+
+client_chat = OpenAI(
+    base_url = URL_CHAT,
+    api_key = 'EMPTY',
+)
+
+def test_chat_text():
+    text_message = [
+        {"role": "user", "content": "What is Python"}
+    ] 
+
+    assert _send_messages(text_message)
+
+def test_chat_image():
+    image = encode_image(Path('flower.jpg'))
+    image_message = [
+        {"role": "user", "content": [{"type": "image_url", "image_url": {"url": image}}]}
+    ]
+    
+    assert _send_messages(image_message)
+
+def test_chat_multi():
+    image = encode_image(Path('flower.jpg'))
+    multi_messages = [
+        {"role": "user", "content": [
+            {"type": "image_url", "image_url": {"url": image}},
+            {"type": "text", "text": "Describe the image."},
+        ]}
+    ]
+
+    assert _send_messages(multi_messages)
+
+def _send_messages(messages):
+    try:
+        response = client_chat.chat.completions.create(
+            model = MODEL_CHAT,
+            messages = messages,
+            temperature = 0.1,
+            max_tokens = 2048,
+            reasoning_effort = "none",
+        )
+        return True
+    
+    except Exception as e:
+        return False
---
+++ tests/vllm/utils.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+from PIL import Image
+from io import BytesIO
+import base64
+
+from typing import Union
+    
+def encode_image(
+        image_repr: Union[Path,Image.Image],
+    ) -> str:
+
+    if isinstance(image_repr,Path):
+        with open(image_repr,'rb') as image_file:
+            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
+            extension = image_repr.suffix[1:]
+    
+    if isinstance(image_repr,Image.Image):
+        image_buffer = BytesIO()
+        extension = image_repr.format.lower()
+        image_repr.save(image_buffer,extension)
+        encoded_string = base64.b64encode(image_buffer.getvalue()).decode('utf-8')
+    
+    return f'data:image/{extension};base64,{encoded_string}'
