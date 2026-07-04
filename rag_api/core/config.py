e05be068239c6bc0b94684ebd32b3b54014c225b
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2026-07-03 23:41:57 -0600

    Migration to use ChromaDB as vector_store
    Changes include:
    - Add necessary env vars to connect to ChromaDB
    - Modifications to use Chroma as vector store instead of In memory

--- rag_api/core/config.py
+++ rag_api/core/config.py
@@ -1,14 +1,16 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    # EMBEDDING_SERVICE_URL: str = "http://embedding-service:8001"
     EMBEDDING_SERVICE_URL: str = "http://127.0.0.1:8001"
     EMBEDDING_SERVICE_APIKEY: str = "EMPTY"
     EMBEDDING_BATCH_SIZE: int = 5
 
-    # RERANKER_SERVICE_URL: str = "http://reranker-service:8002"
     RERANKER_SERVICE_URL: str = "http://127.0.0.1:8002"
     RERANKER_SERVICE_APIKEY: str = "EMPTY"
+
+    CHROMA_HOST: str = "127.0.0.1"
+    CHROMA_PORT: int = 7890
+    CHROMA_TOKEN: str = "efb53c4aa6952909dad18ee9bbe134922d339c4bea78fb757348f5b6f780840c"
     
     DOCUMENTS_TO_RETRIEVE: int = 10
     TOP_K_DOCUMENTS: int = 3
--- rag_api/database/vector_store.py
+++ rag_api/database/vector_store.py
@@ -1,4 +1,5 @@
-from langchain_core.vectorstores import InMemoryVectorStore , VectorStore
+from langchain_chroma import Chroma
+from langchain_core.vectorstores import VectorStore
 
 from rag_api.models.embedding import EmbeddingModel
 from rag_api.core.config import settings
@@ -6,15 +7,20 @@ from rag_api.core.config import settings
 def get_vector_store() -> VectorStore:
     """
     Function to get the LangChain's vector store interface 
-    based on the initialize embedding model with in memory 
-    storage.
+    based on the initialize embedding model and ChromaDB connection.
 
-    Returns
-        vector_store (VectorStore): In memory storage vector store initialize with the embedding model
+    Returns:
+        vector_store (VectorStore): ChromaDB vector store initialize with the embedding model
     """
 
-    vector_store = InMemoryVectorStore(
-        embedding = EmbeddingModel()
+    vector_store = Chroma(
+        embedding_function = EmbeddingModel(),
+        host = settings.CHROMA_HOST,
+        port = settings.CHROMA_PORT,
+        headers = {'X-Chroma-Token': settings.CHROMA_TOKEN},
+        collection_name = "rag_collection",
+        create_collection_if_not_exists = True,
+        collection_metadata = {"hnsw:space": "cosine"},
     )
 
     return vector_store
